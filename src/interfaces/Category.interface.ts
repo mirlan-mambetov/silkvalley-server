eaabe9b8e6787034848dc16ee99a7ff1010f95bd
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-02-04 18:50:39 +0600

    update

--- src/interfaces/Category.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IBase } from './Base.interface'
-import { IProduct } from './Product.interface'
-
-export interface ICategory extends IBase {
-  name: string
-  slug: string
-
-  product: IProduct[]
-}
