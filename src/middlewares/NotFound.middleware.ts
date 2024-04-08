f3aeb47227b3e5dd60ef50d75d4505ed3b0eee28
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-04-08 19:13:55 +0600

    changed

--- src/middlewares/NotFound.middleware.ts
+++ src/middlewares/NotFound.middleware.ts
@@ -1,12 +1,9 @@
 // ApiNotFoundMiddleware.ts
 import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
-import { ApiNotFoundFilter } from 'src/filters/Api.notFound.filter'
 
 @Injectable()
 export class ApiNotFoundMiddleware implements NestMiddleware {
-  private readonly filter = new ApiNotFoundFilter()
-
   use(req: Request, res: Response, next: NextFunction) {
     if (req.originalUrl.startsWith('/api/')) {
       next()
