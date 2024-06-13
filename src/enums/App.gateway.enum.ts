c30e24ebd9ccc516aaf70587693b2b14289cd66a
mirlan-mambetov <mirlan.zaiyrbekov@gmail.com>
2024-06-14 01:47:02 +0600

    changed

--- src/enums/App.gateway.enum.ts
+++ src/enums/App.gateway.enum.ts
@@ -1,6 +1,6 @@
 export enum EnumClientHOST {
-  CLIENT = 'http://localhost:3000',
-  DASHBOARD = 'http://localhost:3001',
+  CLIENT = 'https://slkvalley.com',
+  DASHBOARD = 'https://dashboard.slkvalley.com',
 }
 // CLIENT = 'https://slkvalley.com',
 // DASHBOARD = 'https://dashboard.slkvalley.com',
